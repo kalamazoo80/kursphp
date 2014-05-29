3f885c459ae2584dfba5b714a8fb1d6833c25695
kalamazoo80 <jakub@kalamazoo-design.co.uk>
2014-05-30 00:56:34 +0100

    Delete plik.php

--- sklep/plik.php
+++
@@ -1,6 +0,0 @@
-<?php
-      // $tresc = file_get_contents('http://www.onet.pl') or die('Blad na stronie');
-		$text = 'Ala ma kota, a Zosia nie'.PHP_EOL;
-         file_put_contents('plik.txt', $text, FILE_APPEND);
-?>
-
