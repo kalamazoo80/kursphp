7439369f595f4f54195b9ad4b60719150271c729
kalamazoo80 <jakub@kalamazoo-design.co.uk>
2014-05-30 00:56:13 +0100

    Delete funkcje.php

--- sklep/funkcje.php
+++
@@ -1,9 +0,0 @@
-<?php
-function licznik(){
-        $value = isset($_COOKIE['licznik']) ? ++$_COOKIE['licznik'] : 1;
-        //$value = 1;
-        //$value = ++$_COOKIE['licznik'];
-        setcookie('licznik', $value, time()+3600);
-        return $value;
-}
-?>
